cf323442a858dfaec2688399e08aa71c1310f0c9
pablo-16 <pawel.tkocz@vp.pl>
2017-04-27 21:12:49 +0200

    Prace z quizem

---
+++ quiz.js
@@ -0,0 +1,20 @@
+function sprawdz(){
+	var dobrze = 0;
+	var questions = [1, 2, 3, 4];
+	var ansewrs = [2, 0, 0, 1];
+
+	for (var i=0; i<questions.length; i++){
+		var fn = "pytanie"+questions[i];
+		var f = document.getElementsByName(fn);
+		if(f[0].odp[ansewrs[i]].checked==true){
+			dobrze ++;
+		}
+	}
+
+	if (dobrze == questions.length){
+		alert("OK");
+	}
+	else {
+			alert("NIE! "+dobrze);
+	}
+}
